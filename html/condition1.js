bf984d63b2295d3d31d13823ee7e22ee29f7825a
OSU Phonetics <koehnlein.3@osu.edu>
2020-07-31 11:39:59 -0700

    Fixed full run

--- html/condition1.js
+++ html/condition1.js
@@ -1435,7 +1435,7 @@ function train1blocksLoopBegin(thisScheduler) {
   // set up handler to look after randomisation of conditions etc
   train1blocks = new TrialHandler({
     psychoJS: psychoJS,
-    nReps: 0, method: TrialHandler.Method.SEQUENTIAL,
+    nReps: 1, method: TrialHandler.Method.SEQUENTIAL,
     extraInfo: expInfo, originPath: undefined,
     trialList: 'aoiConditions/train1LoopCondition.xlsx',
     seed: undefined, name: 'train1blocks'
