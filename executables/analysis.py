a5a05c2b1cd4e2e5418fed259fc5135ac3bc6e49
OSU Phonetics <koehnlein.3@osu.edu>
2020-08-14 21:41:49 -0700

    test new conditions:

--- executables/analysis.py
+++ executables/analysis.py
@@ -1,118 +1,120 @@
 from tkinter import Tk
 from tkinter.filedialog import askopenfilenames, askdirectory
 import pandas as pd
-import ntpath
-import pkg_resources.py2_warn
+from ntpath import basename
+#import pkg_resources.py2_warn
 
 def calculateAccuracy(df, phase):
-    if phase + "Response.keys" in df.columns:
-        audio = df[phase + 'Response.keys']
-        questionIndexes = []
-        for i in range(0, len(audio)):
-            if type(audio[i]) == str:
+    allPhases = df['currentPhase']
+    questionIndexes = []
+    for i in range(0, len(allPhases)):
+        if type(allPhases[i]) == str:
+            if phase in allPhases[i]:
                 questionIndexes.append(i)
-        empty = 0
-        correctAnswers = 0
-        total = 0
-        
-        correct = []
-        reactionTime = []
-        
-        #These are the arrays for train phases
-        audio = []
-        trueImage = []
-        falseImage = []
-        
-        #These are the arrays for test phases
-        images = []
-        audio1 = []
-        audio2 = []
-        trueAudio = []
-        
-        #These are the arrays for the post test phase
-        audioA = []
-        audioX = []
-        audioB = []
-        trueAudio = []
-        
-        for index in questionIndexes:
-            
-            total += 1
-            if "train" in phase:
-                audio.append(df['audioTrue'][index].split('/')[1])
-                trueImage.append(df['imageTrue'][index].split('/')[1])
-                falseImage.append(df['imageFalse'][index].split('/')[1])
-                reactionTime.append(df[phase + 'Response.rt'][index])
-                if df[phase + 'Response.keys'][index] == 'None':
-                    empty += 1
-                    correct.append("None")
-                elif df['truePos'][index] == -0.5 and df[phase + 'Response.keys'][index] == 'f':
-                    correctAnswers += 1
-                    correct.append(True)
-                elif df['truePos'][index] == 0.5 and df[phase + 'Response.keys'][index] == 'j':
-                    correctAnswers += 1
-                    correct.append(True)
-                else:
-                    correct.append(False)
-                data = {"Audio": audio, "True Image": trueImage, "False Image": falseImage,
-                    "Correct": correct, "Reaction Time": reactionTime}
-                columns = ["Audio", "True Image", "False Image", "Correct", "Reaction Time"]
-            elif "test" in phase:
-                images.append(df['imageLoc'][index].split('/')[1])
-                audio1.append(df['firstAudio'][index].split('/')[1])
-                audio2.append(df['secondAudio'][index].split('/')[1])
-                trueAudio.append(df['whichAudio'][index])
-                reactionTime.append(df[phase + 'Response.rt'][index])
-                if df[phase + 'Response.keys'][index] == 'None':
-                    empty += 1
-                    correct.append("None")
-                elif df['whichAudio'][index] == 1 and df[phase + 'Response.keys'][index] == 'f':
-                    correctAnswers += 1
-                    correct.append(True)
-                elif df['whichAudio'][index] == 2 and df[phase + 'Response.keys'][index] == 'j':
-                    correctAnswers += 1
-                    correct.append(True)
-                else:
-                    correct.append(False)
-                data = {"Images": images, "Audio 1": audio1, "Audio 2": audio2, "True Audio": trueAudio,
-                    "Correct": correct, "Reaction Time": reactionTime}
-                columns = ["Images", "Audio 1", "Audio 2", "True Audio", "Correct", "Reaction Time"]
-            elif "postTest" in phase:
-                audioA.append(df['audioA'][index].split('/')[1])
-                audioX.append(df['audioX'][index].split('/')[1])
-                audioB.append(df['audioB'][index].split('/')[1])
-                trueAudio.append(df['correctLabel'][index])
-                reactionTime.append(df[phase + 'Response.rt'][index])
-                if df[phase + 'Response.keys'][index] == 'None':
-                    empty += 1
-                    correct.append("None")
-                elif df['correctLabel'][index] == "audioA" and df[phase + 'Response.keys'][index] == 'f':
-                    correctAnswers += 1
-                    correct.append(True)
-                elif df['correctLabel'][index] == "audioB" and df[phase + 'Response.keys'][index] == 'j':
-                    correctAnswers += 1
-                    correct.append(True)
-                else:
-                    correct.append(False)
-                data = {"Audio A": audioA, "Audio X": audioX, "Audio B": audioB, "True Audio": trueAudio,
-                    "Correct": correct, "Reaction Time": reactionTime}
-                columns = ["Audio A", "Audio X", "Audio B", "True Audio", "Correct", "Reaction Time"]
-            else:
-                print("Invalid phase")
-        
-        df = pd.DataFrame(data, columns=columns)
-        
-        print("Phase: " + phase.title())
-        print("Correct: ", correctAnswers)
-        print("Unanswered: ", empty)
-        print("Total: ", total)
-        print("Percentage Correct: ", str(100*(correctAnswers/total)) + "%")
-        print()
-        
-        return df, correctAnswers, empty, total, str(round(100*(correctAnswers/total), 2)) + "%"
-    else:
+    if len(questionIndexes) == 0:
         return pd.DataFrame(), "N/A", "N/A", "N/A", "N/A"
+                
+    empty = 0
+    correctAnswers = 0
+    total = 0
+    
+    correct = []
+    reactionTime = []
+    
+    #These are the arrays for train phases
+    audio = []
+    trueImage = []
+    falseImage = []
+    
+    #These are the arrays for test phases
+    images = []
+    audio1 = []
+    audio2 = []
+    trueAudio = []
+    
+    #These are the arrays for the post test phase
+    audioA = []
+    audioX = []
+    audioB = []
+    trueAudio = []
+    
+    for index in questionIndexes:
+        
+        total += 1
+        if "train" in phase:
+            audio.append(df['audioTrue'][index].split('/')[1])
+            trueImage.append(df['imageTrue'][index].split('/')[1])
+            falseImage.append(df['imageFalse'][index].split('/')[1])
+            reactionTime.append(df[phase + 'Response.rt'][index])
+            if type(df[phase + 'Response.keys'][index]) != str:
+                empty += 1
+                correct.append("None")
+            elif df['truePos'][index] == -0.5 and df[phase + 'Response.keys'][index] == 'f':
+                correctAnswers += 1
+                correct.append(True)
+            elif df['truePos'][index] == 0.5 and df[phase + 'Response.keys'][index] == 'j':
+                correctAnswers += 1
+                correct.append(True)
+            else:
+                correct.append(False)
+            data = {"Audio": audio, "True Image": trueImage, "False Image": falseImage,
+                "Correct": correct, "Reaction Time": reactionTime}
+            columns = ["Audio", "True Image", "False Image", "Correct", "Reaction Time"]
+        elif "test" in phase:
+            images.append(df['imageLoc'][index].split('/')[1])
+            audio1.append(df['firstAudio'][index].split('/')[1])
+            audio2.append(df['secondAudio'][index].split('/')[1])
+            trueAudio.append(df['whichAudio'][index])
+            reactionTime.append(df[phase + 'Response.rt'][index])
+            if type(df[phase + 'Response.keys'][index]) != str:
+                empty += 1
+                correct.append("None")
+            elif df['whichAudio'][index] == 1 and df[phase + 'Response.keys'][index] == 'f':
+                correctAnswers += 1
+                correct.append(True)
+            elif df['whichAudio'][index] == 2 and df[phase + 'Response.keys'][index] == 'j':
+                correctAnswers += 1
+                correct.append(True)
+            else:
+                correct.append(False)
+            data = {"Images": images, "Audio 1": audio1, "Audio 2": audio2, "True Audio": trueAudio,
+                "Correct": correct, "Reaction Time": reactionTime}
+            columns = ["Images", "Audio 1", "Audio 2", "True Audio", "Correct", "Reaction Time"]
+        elif "postTest" in phase:
+            audioA.append(df['audioA'][index].split('/')[1])
+            audioX.append(df['audioX'][index].split('/')[1])
+            audioB.append(df['audioB'][index].split('/')[1])
+            trueAudio.append(df['correctLabel'][index])
+            reactionTime.append(df[phase + 'Response.rt'][index])
+            if type(df[phase + 'Response.keys'][index]) != str:
+                empty += 1
+                correct.append("None")
+            elif df['correctLabel'][index] == "audioA" and df[phase + 'Response.keys'][index] == 'f':
+                correctAnswers += 1
+                correct.append(True)
+            elif df['correctLabel'][index] == "audioB" and df[phase + 'Response.keys'][index] == 'j':
+                correctAnswers += 1
+                correct.append(True)
+            else:
+                correct.append(False)
+            data = {"Audio A": audioA, "Audio X": audioX, "Audio B": audioB, "True Audio": trueAudio,
+                "Correct": correct, "Reaction Time": reactionTime}
+            columns = ["Audio A", "Audio X", "Audio B", "True Audio", "Correct", "Reaction Time"]
+        else:
+            print("Invalid phase")
+    
+    df = pd.DataFrame(data, columns=columns)
+    
+    print("Phase: " + phase.title())
+    print("Correct: ", correctAnswers)
+    print("Unanswered: ", empty)
+    print("Total: ", total)
+    print("Percentage Correct: ", str(100*(correctAnswers/total)) + "%")
+    print()
+    
+    return df, correctAnswers, empty, total, str(round(100*(correctAnswers/total), 2)) + "%"
 
+print("Here")
 Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
 filenames = askopenfilenames(title='Choose file(s)', filetypes = (("CSV Files", "*.csv"),)) # show an "Open" dialog box and return the path to the selected file
 toFolder = askdirectory(title="Choose output folder")
@@ -131,7 +133,7 @@ for filename in filenames:
     phaseDict = {"train1": "Train 1", "train2": "Train 2", "test1": "Test 1",
               "test2": "Test 2", "test3": "Test 3", "postTest": "PostTest"}
     
-    writer = pd.ExcelWriter(toFolder + "/" + ntpath.basename(filename)[:-4] + '_results.xlsx', engine='xlsxwriter')
+    writer = pd.ExcelWriter(toFolder + "/" + basename(filename)[:-4] + '_results.xlsx', engine='xlsxwriter')
     for phase in phaseDict.keys():    
         dfPhase, correct, empty, total, percent = calculateAccuracy(df, phase)
         phases.append(phaseDict[phase])
